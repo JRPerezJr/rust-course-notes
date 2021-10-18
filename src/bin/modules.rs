e48fa599301b55207c4cc8dbccc90f096440eb85
Juan <pront23@gmail.com>
2021-10-18 18:56:08 +0900

    completed managing code

---
+++ src/bin/modules.rs
@@ -0,0 +1,16 @@
+mod greet {
+    pub fn hello() {
+        println!("hello");
+    }
+
+    pub fn goodbye() {
+        println!("goodbye");
+    }
+}
+
+fn main() {
+    use greet::hello;
+    hello();
+
+    greet::goodbye();
+}
